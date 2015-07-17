54d5be011a224a63774d830fcba7fb000973455c
mayank <mayaverm@in.ibm.com>
2015-07-17 16:56:03 +0530

    Initial Commit

---
+++ MFP_Test_Application/apps/MFP_Test_Application/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":2543807617,"date":1437122083467,"machine":"ADMINIB-S8VLV0Q.in.ibm.com"};
+/* Date: Fri Jul 17 14:04:43 IST 2015 */
---
+++ MFP_Test_Application/apps/Test_App2/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":3073642515,"date":1437130432891,"machine":"ADMINIB-S8VLV0Q.in.ibm.com"};
+/* Date: Fri Jul 17 16:23:52 IST 2015 */
